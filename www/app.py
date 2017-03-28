dcb885ecfb30e6a63c8fd84d23527d9c1468ebe0
skiming <skiming@126.com>
2017-03-28 23:47:08 +0800

    add monitor

---
+++ www/app.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author: skiming
+# @Date:   2017-03-28 23:12:04
+# @Last Modified by:   skiming
+# @Last Modified time: 2017-03-28 23:45:40
+#           webapp ¹Ç¼Ü
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+	return web.Response(body=b'<h1>BilibiliFansWebapp</h1>', content_type='text/html')
+
+async def init(loop):
+	app = web.Application(loop=loop)
+	app.router.add_route('GET','/', index)
+	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+	return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
