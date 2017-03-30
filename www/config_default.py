b036c4bccfa594d53ec5a6e4bbcf5096de5b2258
skiming <skiming@126.com>
2017-03-30 16:32:53 +0800

    add configs

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Author: skiming
+# @Date:   2017-03-30 16:26:27
+# @Last Modified by:   skiming
+# @Last Modified time: 2017-03-30 16:32:23
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'bilibilifans'
+    },
+    'session': {
+        'secret': 'BiLiFaNs'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: skiming
+# @Date:   2017-03-30 16:26:43
+# @Last Modified by:   skiming
+# @Last Modified time: 2017-03-30 16:30:45
+
+configs = {
+	
+}
