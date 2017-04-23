a663c4f62f7f00dbedd2b83ded1c27e866e7a934
skiming <skiming@126.com>
2017-04-23 22:38:56 +0800

    modify app construction

---
+++ conf/webpack.config.js
@@ -0,0 +1,65 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        'received-sheet': './js/received-sheet.js',
+        'my-sheet': './js/my-sheet.js',
+        'my-template': './js/my-template.js'
+    },
+  output: {
+    publicPath: '/dist/',
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name]-bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    // noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
